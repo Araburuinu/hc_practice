3be89fd07544e122618e358c872eaf7449d8d4e1
Araburuinu <araburuinu11@gmail.com>
2023-10-28 23:23:36 +0900

    Revert "calendar.rbのコンフリクトを修正"
    This reverts commit 1cb5e718b48be275e481090bd804741f2b40b57c.

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -8,20 +8,19 @@ WDAYS_WIDTH = WDAYS.length * 2 + WDAYS.length - 1
 
 def title_and_wday(input)
   cal_title = "#{input.strftime('%B')} #{input.year}"
-  puts cal_title.center(WDAYS_WIDTH)
+  puts cal_title.center(20)
   puts "#{WDAYS.join(' ')}"
 end
 
-def create_blank(input)
+def first_day(input)
   first_day = Date.new(input.year, input.month, 1)
-  print "#{' ' * (WDAYS_WIDTH - 2)}" if first_day.wday.zero?
-  print '' if first_day.wday == 1
-  print "#{' '.rjust((first_day.wday - 1) * 3, ' ')}" if first_day.wday >= 2
+  puts "#{' ' * (WDAYS_WIDTH - 2)}#{'1'.rjust(2, ' ')}" if first_day.wday.zero?
+  print "#{' '.rjust((first_day.wday - 1) * 3 - 1, ' ')}#{'1'.rjust(2, ' ')} " if first_day.wday >= 1
 end
 
-def create_days(input)
+def other_days(input)
   end_day = Date.new(input.year, input.month, -1)
-  (1..end_day.day).each do |d|
+  (2..end_day.day).each do |d|
     that_day = Date.new(input.year, input.month, d)
     adjuster = that_day.wday.zero? ? "\n" : ' '
     print "#{that_day.day.to_s.rjust(2, ' ')}#{adjuster}"
@@ -31,8 +30,8 @@ end
 
 def calendar(input)
   title_and_wday(input)
-  create_blank(input)
-  create_days(input)
+  first_day(input)
+  other_days(input)
 end
 
 opt = OptionParser.new
@@ -41,7 +40,7 @@ opt.parse!(ARGV)
 
 if ARGV[0].nil?
   calendar(Date.today)
-elsif (1..12).cover?(ARGV[0].to_i)
+elsif ARGV[0].to_i >= 1 && ARGV[0].to_i <= 12
   calendar(Date.new(Date.today.year, ARGV[0].to_i, 1))
 else
   print "#{ARGV[0]} is neither a month number (1..12) nor a name"
